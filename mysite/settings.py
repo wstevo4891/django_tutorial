3839595899a0712d60e0f4e45ad117812b76de72
Eric Stephenson <ericb.stephenson@gmail.com>
2025-10-04 15:01:11 -0700

    install django_debug_toolbar

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import sys
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,3 +123,19 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+  '127.0.0.1',
+]
+
+TESTING = 'test' in sys.argv or 'PYTEST_VERSION' in os.environ
+
+if not TESTING:
+  INSTALLED_APPS = [
+    *INSTALLED_APPS,
+    'debug_toolbar',
+  ]
+  MIDDLEWARE = [
+    *MIDDLEWARE,
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
+  ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -14,10 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
+  path('polls/', include('polls.urls')),
+  path('admin/', admin.site.urls),
 ]
+
+if not settings.TESTING:
+  from debug_toolbar.toolbar import debug_toolbar_urls
+
+  urlpatterns = [
+    *urlpatterns,
+  ] + debug_toolbar_urls()
