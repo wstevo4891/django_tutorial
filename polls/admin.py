3a7fdd3bf0e531000e198f98f6e5c00ab17554a1
Eric Stephenson <ericb.stephenson@gmail.com>
2025-10-04 13:32:56 -0700

    update admin interface

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 
 from .models import Choice, Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+  model = Choice
+  extra = 3
 
-admin.site.register(Choice)
+class QuestionAdmin(admin.ModelAdmin):
+  # fields = ["pub_date", "question_text"]
+  fieldsets = [
+    (None, { "fields": ["question_text"] }),
+    ("Date information", { "fields": ["pub_date"], "classes": ["collapse"] })
+  ]
+  inlines = [ChoiceInline]
+  list_display = ["question_text", "pub_date", "was_published_recently"]
+  list_filter = ["pub_date"]
+  search_fields = ["question_text"]
+
+admin.site.register(Question, QuestionAdmin)
